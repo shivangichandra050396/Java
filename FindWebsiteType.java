8882fba71264187911469a7569a25d08b890b808
shivangichandra050396 <163521713+shivangichandra050396@users.noreply.github.com>
2024-08-30 22:54:32 +0530

    Update FindWebsiteType.java

--- FindWebsiteType.java
+++ FindWebsiteType.java
@@ -4,14 +4,14 @@ public static void main(String[] s)
 {
 Scanner sc=new Scanner(System.in)
 String enteredString=sc.nextLine();
-String protocol=enteredString.subString(0, enteredString.indexOf(":"));
+String protocol=enteredString.substring(0, enteredString.indexOf(":"));
 if(protocol="http")
   System.out.println("HTTP");
 else if(protocol="https")
   System.out.println("HTTPS");
 else
   System.out.println("Invalid");
-String type=enteredString.subString(enteredString.lastIndexOf(".")+1);
+String type=enteredString.substring(enteredString.lastIndexOf(".")+1);
 if(ext.equals("com"))
             System.out.println("Commercial");
 else if(ext.equals("org"))
